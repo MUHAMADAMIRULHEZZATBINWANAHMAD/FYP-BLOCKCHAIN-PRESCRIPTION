d091090deedb7b129913e15a59c5bca6fe56fa6e
alifyazid01 <alifburnstt@gmail.com>
2026-04-09 22:09:24 +0800

    apa

---
+++ src/pages/setting.jsx
@@ -0,0 +1,11 @@
+// This is a JSX component
+function Welcome() {
+  const name = "Developer";
+
+  return (
+    <div className="container">
+      <h1>Hello, {name}!</h1>
+      <p>Welcome to your Vite + React app.</p>
+    </div>
+  );
+}
